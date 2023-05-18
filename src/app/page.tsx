6320002e6eea9155b79586c41253103c3c554f5f
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-05-18 16:46:24 -0300

    ✨ feat: Display datas in screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-
+"use client";
+import { useSearch } from "@/context/search.context";
+import { SearchButton } from "@/components/SearchButton";
+import { Card } from "@/components/Card/Card";
 
 export default function Home() {
+  const { cardSearch } = useSearch();
+
   return (
-    <main className='bg-black text-white text-xl'>
-      <h1>Home</h1>
-   </main>
-  )
+    <main className="mt-52 flex h-full w-full justify-center">
+      <section className="flex flex-col items-center gap-20">
+        <SearchButton />
+
+        {cardSearch.cards.length > 0 &&
+          cardSearch.cards.map((card) => (
+            <Card key={card.slug} attributes={card} type="small" />
+          ))}
+      </section>
+    </main>
+  );
 }
