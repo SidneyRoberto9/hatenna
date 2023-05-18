2d9c0408efbe8aa2e3e8b6cfb010835e46689c24
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-05-18 16:42:48 -0300

    ✨ feat: Config tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      width: {
+        118: "29.5rem",
+        892: "223rem",
+      },
+      colors: {
+        primary: "#0d0c0d",
+        secondary: "#ffffff",
+        "primary-button": "#94898e",
+        "secondary-button": "#f3f2f2",
+        Accent: "#a1979b",
       },
     },
   },
   plugins: [],
-}
+};
