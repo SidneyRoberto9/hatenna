129eb90076904daf0bbb777dbfeff45ed73fd204
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-05-20 19:08:04 -0300

    ✨ feat: Add Jikan api instace with axios

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-export const api = axios.create({
+export const kitsuApi = axios.create({
   baseURL: "https://kitsu.io/api/edge/",
+});
+
+export const JikanApi = axios.create({
+  baseURL: " https://api.jikan.moe/v4/",
 });
