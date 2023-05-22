a2af0252007b3105e0254241599cd392a11b352b
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-05-22 01:25:32 -0300

    ✨ feat: Create Pages to login and Register Users

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from '@/components/Form/Login';
+
+export default function Page() {
+  return (
+    <section className="flex h-[70vh] items-center justify-center">
+      <LoginForm />
+    </section>
+  );
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from '@/components/Form/Register';
+
+export default function RegisterPage() {
+  return (
+    <section className="flex h-[70vh] items-center justify-center">
+      <RegisterForm />
+    </section>
+  );
+}
