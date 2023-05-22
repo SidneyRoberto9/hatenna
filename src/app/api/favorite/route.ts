d8e0450861313f8bbd6793342c9720248d1b2075
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-05-22 01:27:36 -0300

    ✨ feat: Create next route to favorite Hatenna anime

---
+++ src/app/api/favorite/route.ts
@@ -0,0 +1,43 @@
+import { prisma } from '@/server/prisma';
+
+export async function POST(request: Request) {
+  const data = await request.json();
+
+  const favorite = await prisma.favoriteAnimes.findFirst({
+    where: {
+      animeSlug: data.slug,
+      userEmail: data.email,
+    },
+  });
+
+  console.log(favorite);
+
+  if (data.type === "add") {
+    if (favorite) {
+      return new Response(JSON.stringify({ isFavorite: true }), {
+        headers: { "content-type": "application/json" },
+      });
+    } else {
+      await prisma.favoriteAnimes.create({
+        data: {
+          animeSlug: data.slug,
+          userEmail: data.email,
+        },
+      });
+      return new Response(JSON.stringify({ isFavorite: true }), {
+        headers: { "content-type": "application/json" },
+      });
+    }
+  } else if (data.type === "remove") {
+    if (favorite) {
+      await prisma.favoriteAnimes.delete({
+        where: {
+          id: favorite.id,
+        },
+      });
+    }
+    return new Response(JSON.stringify({ isFavorite: false }), {
+      headers: { "content-type": "application/json" },
+    });
+  }
+}
