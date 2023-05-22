b7b2c8c02d16c94124dcbe1b214dba51c61a53dc
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-05-22 01:24:25 -0300

    📦 refactor: Change Favorite button to work  alone

--- src/components/Anime/FavoriteButton.tsx
+++ src/components/Anime/FavoriteButton.tsx
@@ -1,18 +1,49 @@
+"use client";
+
+import { useState } from 'react';
+
+import { api } from '@/server/api';
+
 interface FavoriteButtonProps {
   isFavorite: boolean;
+  slug: string;
+  email: string;
 }
 
-export function FavoriteButton({ isFavorite }: FavoriteButtonProps) {
-  if (isFavorite) {
+type method = "add" | "remove";
+
+export function FavoriteButton({
+  isFavorite,
+  slug,
+  email,
+}: FavoriteButtonProps) {
+  const [isFav, setIsFav] = useState<boolean>(isFavorite);
+
+  async function handleFavorite(method: method) {
+    const { data } = await api.post("/favorite", {
+      slug,
+      email,
+      type: method,
+    });
+    setIsFav(data.isFavorite);
+  }
+
+  if (isFav) {
     return (
-      <button className="rounded-lg border border-pink-500 bg-pink-400 p-2 text-secondary transition-all duration-500 hover:border-primary hover:bg-secondary hover:text-primary">
+      <button
+        onClick={() => handleFavorite("remove")}
+        className="rounded-lg border border-pink-500 bg-pink-400 p-2 text-secondary transition-all duration-500 hover:border-primary hover:bg-secondary hover:text-primary"
+      >
         Remove from Favorites
       </button>
     );
   }
 
   return (
-    <button className="rounded-lg border border-primary bg-secondary p-2 text-primary transition-all duration-500 hover:border-pink-500 hover:bg-pink-400 hover:text-secondary">
+    <button
+      onClick={() => handleFavorite("add")}
+      className="rounded-lg border border-primary bg-secondary p-2 text-primary transition-all duration-500 hover:border-pink-500 hover:bg-pink-400 hover:text-secondary"
+    >
       Add to Favorites
     </button>
   );
